3ffde7fa8fa289bc349e9892730649dda1d84219
Jonás-fg <3812317550101@ingenieria.usac.edu.gt>
2026-04-13 18:23:37 -0600

    [ADD] primer commit: Creacion de los proyectos independientes sugeridos por el aux, para la inicializacion del Proyecto 3

---
+++ Proyecto 3/Proyecto_3_IPC2/ITGSA__Frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ITGSA__Frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
